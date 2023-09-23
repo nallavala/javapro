2e4a09aa505143a50b4319480cb72fa4f1cc541e
nallavala <rajeswariramu98@gmail.com>
2023-09-23 12:25:08 +0530

    first java

---
+++ src/Collections/Array.java
@@ -0,0 +1,43 @@
+package Collections;
+
+public class Array {
+	/*
+	 * static void min(int a[]){ int min=a[0]; for(int i=1;i<a.length;i++)
+	 * if(min>a[i]) min=a[i]; System.out.println(min); }
+	 */
+	 
+	 /* 
+	 * int b[]= {2,3,4,5,1}; min(b);
+	 */
+//		  for(int i:a)
+//		  for(int i=0;i<a.length;i++) 
+		 
+		  
+	
+		 
+	/*	int a[]=new int[4];
+		a[0]=10;
+		a[1]=11;
+		a[2]=12;
+		a[3]=13;
+		for(int i=0;i<a.length;i++)
+		System.out.println(a[i]);
+	}*/
+	
+	
+
+//Multidimenstional array
+  
+//public static void main(String args[]){  
+//declaring and initializing 2D array  
+public static void main(String args[]) {
+int arr[][]={{1,2,3},{2,4,5},{4,4,5}}; 
+//printing 2D array  
+for(int i=0;i<3;i++){  
+ for(int j=0;j<3;j++){  
+   System.out.print(arr[i][j]+" ");  
+ }  
+ System.out.println();  
+}  
+}
+}
---
+++ src/Collections/ArrayListExample.java
@@ -0,0 +1,17 @@
+package Collections;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+			 public static void main(String args[]){ 
+			  ArrayList<String> a=new ArrayList<String>(Mango,Apple, Banana, Grapes);//Creating arraylist    
+			      a.add("Mango");//Adding object in arraylist    
+			      a.add("Apple");    
+			      a.add("Banana");    
+			      a.add("Grapes"); 
+			     
+			      //Printing the arraylist object   
+			      System.out.println(a);  
+			 }  
+}
+
