6798976796c8973b9984e8146d9d6507cc2e74ee
nallavala <rajeswariramu98@gmail.com>
2023-09-23 16:05:08 +0530

    newclasspush

---
+++ src/Collections/ArrayNewList.java
@@ -0,0 +1,11 @@
+package Collections;
+
+public class ArrayNewList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is for testing");
+
+	}
+
+}
